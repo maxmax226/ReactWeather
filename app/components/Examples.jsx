8a5e60307ee5f96979f6dbc5e6a7e038852589a0
Max Lam <maxlam@localhost.local>
2017-06-30 23:10:35 +0800

    Rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -8,6 +8,6 @@ const React = require('react');
 //     }
 // });
 
-const Examples = (props) => <h3>Examples Component</h3>;
+const Examples = (props) => <h3>Examples</h3>;
 
 module.exports = Examples;
